c25a28b0a1d41b6ce6e8cd54fa9c6c45ade4713e
SolAguero <solaguero@debian.solchi>
2023-12-11 21:27:28 -0300

    Revert "Examen Final"
    This reverts commit 716d2a80f7bd4bf761b35506bda2385bdedecf21.

--- src/main/java/com/mycompany/presentation/Menu.java
+++ src/main/java/com/mycompany/presentation/Menu.java
@@ -39,7 +39,7 @@ public class Menu extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(153, 0, 153));
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setBackground(new java.awt.Color(102, 0, 102));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
