37071aa041cfdc60a69fe3d50ef288287e63dc05
SolAguero <solaguero@debian.solchi>
2023-12-11 21:15:20 -0300

    Refactor- Final Exam2

--- src/main/java/com/mycompany/presentation/Menu.java
+++ src/main/java/com/mycompany/presentation/Menu.java
@@ -120,7 +120,7 @@ public class Menu extends javax.swing.JFrame {
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
-        new ConsultarCiudad().setVisible(true);
+        new CiudadPresentation().setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
